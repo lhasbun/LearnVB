a527ed95d66fff8ce5445424059e44d9ba63f8ab
Luis Hasbun <41136478+lhasbun@users.noreply.github.com>
2023-02-22 23:00:37 -0600

    Started stopwatch application

--- VB Code/Class 1/TestProject/TestProject/Form1.Designer.vb
+++ VB Code/Class 1/TestProject/TestProject/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frmStopWatch
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,10 +22,122 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtStart = New System.Windows.Forms.TextBox()
+        Me.btnEnd = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtEnd = New System.Windows.Forms.TextBox()
+        Me.txtElapsed = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(12, 12)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(130, 25)
+        Me.btnStart.TabIndex = 0
+        Me.btnStart.Text = "&Start Timing"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(148, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 17)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Start Time"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(148, 47)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(68, 17)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "End Time"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(148, 78)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(130, 17)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Elapsed Time (sec)"
+        '
+        'txtStart
+        '
+        Me.txtStart.Location = New System.Drawing.Point(284, 13)
+        Me.txtStart.Name = "txtStart"
+        Me.txtStart.Size = New System.Drawing.Size(114, 22)
+        Me.txtStart.TabIndex = 7
+        '
+        'btnEnd
+        '
+        Me.btnEnd.Location = New System.Drawing.Point(12, 43)
+        Me.btnEnd.Name = "btnEnd"
+        Me.btnEnd.Size = New System.Drawing.Size(130, 25)
+        Me.btnEnd.TabIndex = 9
+        Me.btnEnd.Text = "&End Timing"
+        Me.btnEnd.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(12, 74)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(130, 25)
+        Me.btnExit.TabIndex = 10
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'txtEnd
+        '
+        Me.txtEnd.Location = New System.Drawing.Point(284, 44)
+        Me.txtEnd.Name = "txtEnd"
+        Me.txtEnd.Size = New System.Drawing.Size(114, 22)
+        Me.txtEnd.TabIndex = 11
+        '
+        'txtElapsed
+        '
+        Me.txtElapsed.Location = New System.Drawing.Point(284, 75)
+        Me.txtElapsed.Name = "txtElapsed"
+        Me.txtElapsed.Size = New System.Drawing.Size(114, 22)
+        Me.txtElapsed.TabIndex = 12
+        '
+        'frmStopWatch
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(425, 113)
+        Me.Controls.Add(Me.txtElapsed)
+        Me.Controls.Add(Me.txtEnd)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnEnd)
+        Me.Controls.Add(Me.txtStart)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnStart)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "frmStopWatch"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Stopwatch Application"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents btnStart As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtStart As TextBox
+    Friend WithEvents btnEnd As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents txtEnd As TextBox
+    Friend WithEvents txtElapsed As TextBox
 End Class
--- VB Code/Class 1/TestProject/TestProject/Form1.vb
+++ VB Code/Class 1/TestProject/TestProject/Form1.vb
@@ -1,3 +1,2 @@
-﻿Public Class Form1
-
+﻿Public Class frmStopWatch
 End Class
